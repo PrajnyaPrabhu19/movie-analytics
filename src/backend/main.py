cddc84737b91b2eba3db583a6be2aff05a71a156
Anish <anish.sekar94@gmail.com>
2020-04-14 23:44:20 -0700

    Feature: searchNumeric() API

--- src/backend/main.py
+++ src/backend/main.py
@@ -4,6 +4,9 @@ from Modules import ParseDataset
 from flask import Flask
 from flask import request
 from flask_cors import CORS
+from flask import jsonify
+
+import json
 
 app = Flask(__name__)
 CORS(app)
@@ -16,6 +19,36 @@ def getData():
 
     print(request.headers)
     return 'Hello Message!'
+
+@app.route('/searchNumeric', methods=['GET']) 
+def search():
+
+    #Get all the parameters from the URL string
+    search_field = request.args.get('search_field')
+    search_query = request.args.get('search_query')
+
+    #Code for operations
+    operations = {
+    '0' : '=',
+    '1' : '>',
+    '2' : '>=',
+    '3' : '<',
+    '4' : '<='
+    }
+    search_inequality = request.args.get('search_inequality')
+
+    responseObject = []
+
+    for movie in moviesData:
+        try:
+            if (eval(movie[search_field]+operations[search_inequality]+search_query)):
+                responseObject.append(movie)
+
+        except Exception as e:
+            print(e)
+
+    return jsonify(responseObject)
+
 
 
 if __name__ == '__main__':
