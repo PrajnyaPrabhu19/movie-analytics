2477aa84cae93163a4100aef4af7fbe126724717
Jacob Halvorson <jhalvorson6687@icloud.com>
2020-05-03 19:35:59 -0700

    added import/export file

---
+++ src/backend/Modules/ImportExport.py
@@ -0,0 +1,12 @@
+import os
+##
+    ## This file contains methods to support import/export functionality
+##
+
+def importData():
+    filepath = os.path.abspath(os.path.dirname(os.path.abspath(__file__))) + "/Test/Data/test_dataset.csv"
+    return
+
+def exportData():
+
+    return
